a326f89ff7e2ce46c87ecf824b7f5357e78236af
Pongchanan <pongchanansriwanna2547@gmail.com>
2024-12-26 02:13:08 +0700

    Enhance audio processing by adding speech recognition and exporting audio chunks as WAV files

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from pydub import AudioSegment, silence
+import streamlit as st
+import speech_recognition as sr
+
+recog = sr.Recognizer()
 
 # Specify the exact path to the ffmpeg.exe file
 AudioSegment.converter = "C:/Users/pongc/Downloads/ffmpeg-master-latest-win64-gpl/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe"
-
-import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Audio to text converter</h1>", unsafe_allow_html=True)
 st.markdown("---")
@@ -11,7 +13,14 @@ st.markdown("---")
 audio = st.file_uploader("Upload your audio file", type=["mp3", "wav"])
 if audio:
     audio_segment = AudioSegment.from_file(audio)
-    chunks = silence.split_on_silence(audio_segment, min_silence_len=100, silence_thresh=audio_segment.dBFS-20, keep_silence=100)
+    chunks = silence.split_on_silence(audio_segment, min_silence_len=500, silence_thresh=audio_segment.dBFS-20, keep_silence=100)
     
-    for chunk in chunks:
-        print(chunk)
+    for index, chunk in enumerate(chunks):
+        chunk.export(str(index)+".wav", format="wav")
+        with sr.AudioFile(str(index)+".wav") as source:
+            recorded = recog.record(source)
+            try:
+                text = recog.recognize_google(recorded)
+                print(text)
+            except:
+                print("None")
